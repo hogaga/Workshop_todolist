09c7e71e4cc1c1eee9b3509c3ba5b2973d919581
hoga1999 <n25280382@gmail.com>
2021-04-20 14:05:27 +0800

    第一步驟

--- js-todo-list/main.js
+++ js-todo-list/main.js
@@ -1,5 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('ul').addEventListener('click',function(e) {
+  document.querySelector('ul').addEventListener('click', function(e) {
     e.target.classList.toggle('checked')
   })
+
+  document.tgetElemenById('close').addEventListener('click', function(e) {
+    e.target.remove('li')
+  })
+
+
+
+
 })
